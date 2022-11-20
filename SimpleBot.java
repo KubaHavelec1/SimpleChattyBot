32028d08e11526328ce28007df7ae0ff4bdfcd10
Jakub Havelka <83947945+KubaHavelec1@users.noreply.github.com>
2022-11-20 20:22:29 +0100

    I translated my code to English

--- SimpleBot.java
+++ SimpleBot.java
@@ -6,37 +6,37 @@ public class SimpleBot {
     final static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        pozdrav("Kamilator", "2022");
-        zadejJmeno();
-        uhodniVek();
-        pocitej();
+        greet("Kamilator", "2022");
+        tellMeYourName();
+        guessAge();
+        count();
         test();
 
     }
 
-    static void pozdrav(String botJmeno, String rokVytvoreni) {
-        System.out.println("Ahoj! Me jmeno je " + botJmeno + ".");
-        System.out.println("Byl jsem vytvoren " + rokVytvoreni + ".");
-        System.out.println("Jak se jmenujes ty?");
+    static void greet(String botJmeno, String rokVytvoreni) {
+        System.out.println("Hello! My name is " + botJmeno + ".");
+        System.out.println("I was created " + rokVytvoreni + ".");
+        System.out.println("What's your name?");
     }
 
-    static void zadejJmeno() {
+    static void tellMeYourName() {
         String name = scanner.nextLine();
-        System.out.println("Rad te poznavam, " + name + "!");
+        System.out.println("Nice to meet you, " + name + "!");
     }
 
-    static void uhodniVek() {
-        System.out.println("Schvalne , kolik ti asi je.");
-        System.out.println("Zadej zbytky po deleni tveho veku 3, 5 a 7.");
+    static void guessAge() {
+        System.out.println("Let me guess your age.");
+        System.out.println("Enter the remainders of dividing your age by 3, 5 and 7.");
         int rem3 = scanner.nextInt();
         int rem5 = scanner.nextInt();
         int rem7 = scanner.nextInt();
         int age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105;
-        System.out.println("Je ti " + age + "; To je spravny cas zacit programovat!");
+        System.out.println("Your age is " + age + " that's a good time to start programming!");
     }
 
-    static void pocitej() {
-        System.out.println("Ted ti ukazu , ze umim pocitat do jakehokoliv cisla.");
+    static void count() {
+        System.out.println("Now I will prove to you that I can count to any number you want.");
         int num = scanner.nextInt();
         for (int i = 0; i <= num; i++) {
             System.out.printf("%d!\n", i);
@@ -44,23 +44,23 @@ public class SimpleBot {
     }
 
     static void test() {
-        int odpoved;
-        System.out.println("Jdeme otestovat tvoji znalost programovani.");
-        System.out.println("Proc pouzivame metody?");
-        System.out.println("1. Abychom opakovali prikaz vicekrat.");
-        System.out.println("2. Abychom rozlozili program na nekolik malych podprogramu.");
-        System.out.println("3. Abychom urcili dobu provedeni programu.");
-        System.out.println("4. Abychom prerusili provadeni programu");
+        int answer;
+        System.out.println("Let's test your programming knowledge.");
+        System.out.println("Why do we use methods?");
+        System.out.println("1. To repeat a statement multiple times.");
+        System.out.println("2. To decompose a program into several small subroutines.");
+        System.out.println("3. To determine the execution time of a program.");
+        System.out.println("4. To interrupt the execution of a program.");
 
         do {
-          odpoved = scanner.nextInt();
-          if (odpoved != 2) {
-              System.out.println("Spatne , zkus to znovu");
+          answer = scanner.nextInt();
+          if (answer != 2) {
+              System.out.println("Please, try again.");
 
           } else {
-              konec();
+              end();
           }
-        } while (odpoved != 2);
+        } while (answer != 2);
 
 
 
@@ -68,7 +68,7 @@ public class SimpleBot {
 
     }
 
-    static void konec() {
-        System.out.println("Vyborne , preju hezky den!");
+    static void end() {
+        System.out.println("Congratulations, have a nice day!");
     }
 }
